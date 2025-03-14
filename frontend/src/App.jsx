2c58d7af739269d1497fd5b1c45d9caac8618856
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-14 14:44:04 +0530

    changed home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Navbar from './components/shared/Navbar';
 import Login from './components/auth/login';
 import Signup from './components/auth/signup';
-import Home from './components/Home';
 import Jobs from './components/Jobs';
+import Home  from "./components/Home";
 import Browse from './components/Browse';
 import Profile from './components/Profile';
 import Companies from './components/admin/Companies';
