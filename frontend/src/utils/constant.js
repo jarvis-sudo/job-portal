b21016ce57ada34f1c21175314d9a4d23d603004
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-18 19:07:09 +0530

    liink

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -6,7 +6,7 @@ export const APPLICATION_API_END_POINT = "http://loclahost:8000/api/v1/applicati
 
 
 
-export const USER_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/user";
+export const USER_API_END_POINT = "https://https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ";
 //export const COMPANY_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/company";
 //export const JOB_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/job";
 //export const APPLICATION_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/application";
