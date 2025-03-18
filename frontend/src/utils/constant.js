d287d2b1fdcaf472b094746c95a8449762df37fd
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-18 19:27:38 +0530

    links2

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -6,10 +6,10 @@
 
 
 
-export const USER_API_END_POINT = "https://https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/user";
-export const COMPANY_API_END_POINT = "https://https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/company";
-export const JOB_API_END_POINT = "https://https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/application";
+export const USER_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/user";
+export const COMPANY_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/company";
+export const JOB_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/application";
 
 
 
