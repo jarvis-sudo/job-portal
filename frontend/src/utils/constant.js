100b732a7d9bc8667b49980de662aa533bc822ae
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-19 16:00:51 +0530

    links3

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,9 +4,9 @@
 
 //export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
 
+//https://backend-gamma-ecru-68.vercel.app/
 
-
-export const USER_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/user";
+export const USER_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/user";
 export const COMPANY_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/company";
 export const JOB_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/job";
 export const APPLICATION_API_END_POINT = "https://vercel.com/jarvis-sudos-projects/backend/9czF1L2pCsFDh11NTLeAfaVkryyJ/api/v1/application";
