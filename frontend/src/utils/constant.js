6e517250ca29ec9c2bbecb45a6199aec5fa391fc
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-20 18:47:48 +0530

    env

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,15 +1,15 @@
-//export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
-//export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-//export const APPLICATION_API_END_POINT = "http://loclahost:8000/api/v1/application"
+export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const APPLICATION_API_END_POINT = "http://loclahost:8000/api/v1/application"
 
-//export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
 
 //https://backend-gamma-ecru-68.vercel.app/
 
-export const USER_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/user";
-export const COMPANY_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/company";
-export const JOB_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/application";
+//export const USER_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/user";
+//export const COMPANY_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/company";
+//export const JOB_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/job";
+//export const APPLICATION_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/application";
 
 
 
