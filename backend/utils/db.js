ab6b594ebf7c5cadcd13e31c721b2d6fca0fdc56
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-21 00:00:14 +0530

    links

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
         console.log("Mongo URI:", process.env.MONGO_URI);
 
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 10000, // Wait 10s before timeout
             socketTimeoutMS: 45000, // Keep sockets open for 45s
         });
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,8 @@
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://loclahost:8000/api/v1/application"
+//export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+//export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+//export const APPLICATION_API_END_POINT = "http://loclahost:8000/api/v1/application"
 
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+//export const USER_API_END_POINT= "http://localhost:8000/api/v1/user";
 
 //https://backend-gamma-ecru-68.vercel.app/
 
