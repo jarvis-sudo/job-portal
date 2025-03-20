69d2aaf9fa08a099c93da6f9449d22e8317a7097
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-20 12:32:02 +0530

    debugging

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
 
         try {
           dispatch(setLoading(true));
-          const res = await axios.post(`${USER_API_END_POINT}/login`, input , {
+          const res = await axios.post("https://backend-gamma-ecru-68.vercel.app/api/v1/user/login", input , {
             headers : {
               "Content-Type" : "application/json"
             },
