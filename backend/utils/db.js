842ee02c2b0a0d327ba2f81bd30f95b9a34da235
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-20 14:15:00 +0530

    links

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-     //   console.log("Mongo URI:", process.env.MONGO_URI);
+        console.log("Mongo URI:", process.env.MONGO_URI);
 
         await mongoose.connect(process.env.MONGO_URI);
         console.log("mongodb connected successfully")
