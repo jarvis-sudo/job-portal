42e159ba7fb3b3b034e54091bcbabae3fc69ff4f
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-20 23:21:40 +0530

    db

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,7 +5,12 @@ const connectDB = async () => {
     try {
         console.log("Mongo URI:", process.env.MONGO_URI);
 
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000, // Wait 10s before timeout
+            socketTimeoutMS: 45000, // Keep sockets open for 45s
+        });
         console.log("mongodb connected successfully")
     } catch (error) {
         console.error("Error , connecting to mongo db!",error.message);
