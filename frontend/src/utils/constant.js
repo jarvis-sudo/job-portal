fc53428fc6221331f11732e1fdc80eabd4df9972
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-21 12:21:31 +0530

    links

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -6,12 +6,15 @@
 
 //https://backend-gamma-ecru-68.vercel.app/
 
-//export const USER_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/user";
-//export const COMPANY_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/company";
-//export const JOB_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/job";
-//export const APPLICATION_API_END_POINT = "https://backend-gamma-ecru-68.vercel.app/api/v1/application";
+export const USER_API_END_POINT = "https://backend-tan-three.vercel.app/api/v1/user";
+export const COMPANY_API_END_POINT = "https://backend-tan-three.vercel.app/api/v1/company";
+export const JOB_API_END_POINT = "https://backend-tan-three.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://backend-tan-three.vercel.app/api/v1/application";
 
 
 
 
 //https://backend-gamma-ecru-68.vercel.app/api/v1/user/login
+
+
+//https://backend-tan-three.vercel.app/
