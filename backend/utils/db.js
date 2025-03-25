b6ba5aec1acb4afeef6ef1c5e35edc5632db7628
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-03-25 14:58:54 +0530

    db

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,15 +5,19 @@ const connectDB = async () => {
     try {
         console.log("Mongo URI:", process.env.MONGO_URI);
 
+        if (!process.env.MONGO_URI) {
+            throw new Error("❌ MONGO_URI is not defined. Check your environment variables.");
+        }
+
         const conn =  await mongoose.connect(process.env.MONGO_URI, {
             serverSelectionTimeoutMS: 10000, // Wait 10s before timeout
             socketTimeoutMS: 45000, // Keep sockets open for 45s
         });
-        console.log("mongodb connected successfully")
+      //  console.log("mongodb connected successfully")
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("Error , connecting to mongo db!",error.message);
-        process.exit(1);
+        //process.exit(1);
     }
 }
 
